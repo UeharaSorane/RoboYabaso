80e27bc0d453ad88e59d3b85464d78d68aa4b3eb
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-06 21:14:14 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -3,7 +3,7 @@
 require('fs').readdirSync('./roll/').forEach(function(file) {
 	if (file.match(/\.js$/) !== null && file !== 'index.js') {
 	  var name = file.replace('.js', '');
-	  exports[name] = require('../mainfunction/' + file);
+	  exports[name] = require('../roll/' + file);
 	}
   });
 
